Fix shift, variance, trend generation

agots/multivariate_generators/multivariate_extreme_outlier_generator.py
@@ -4,14 +4,14 @@ from .base import MultivariateOutlierGenerator
 
 
 class MultivariateExtremeOutlierGenerator(MultivariateOutlierGenerator):
-    def __init__(self, timestamps=None, value=1):
+    def __init__(self, timestamps=None, factor=1):
         self.timestamps = [] if timestamps is None else list(sum(timestamps, ()))
-        self.value = value
+        self.factor = factor
 
     def get_value(self, current_timestamp, timeseries):
         if current_timestamp in self.timestamps:
             local_std = timeseries.iloc[max(0, current_timestamp - 10):current_timestamp + 10].std()
-            return np.random.choice([-1, 1]) * self.value * local_std
+            return np.random.choice([-1, 1]) * self.factor * local_std
         else:
             return 0
 

agots/multivariate_generators/multivariate_shift_outlier_generator.py
@@ -4,35 +4,14 @@ from .base import MultivariateOutlierGenerator
 
 
 class MultivariateShiftOutlierGenerator(MultivariateOutlierGenerator):
-    def __init__(self, timestamps=None, value=None, shift_value=None):
+    def __init__(self, timestamps=None, factor=5):
         timestamps = timestamps or []
         self.timestamps = timestamps
-        self.value = value
-        self.SHIFT_VALUE = shift_value
-        self.MEAN_FACTOR = 5
-        self.ongoing_shift = False
-
-    def get_value(self, current_timestamp, timeseries):
-        start_timestamps = [start for start, _ in self.timestamps]
-        end_timestamps = [end for _, end in self.timestamps]
-
-        if current_timestamp in end_timestamps:
-            self.ongoing_shift = False
-
-        if current_timestamp in start_timestamps or self.ongoing_shift:
-            self.ongoing_shift = True
-            if self.value is None:
-                if self.SHIFT_VALUE is None:
-                    local_mean = timeseries.iloc[max(0, current_timestamp - 10):current_timestamp + 10].mean()
-                    self.SHIFT_VALUE = local_mean * self.MEAN_FACTOR
-                return np.random.random() + self.SHIFT_VALUE
-            else:
-                return self.value
-        else:
-            return 0
+        self.factor = factor
 
     def add_outliers(self, timeseries):
-        additional_values = []
-        for timestamp_index in range(len(timeseries)):
-            additional_values.append(self.get_value(timestamp_index, timeseries))
+        additional_values = np.zeros(timeseries.size)
+        for start, end in self.timestamps:
+            local_std = timeseries.iloc[max(0, start - 10):end + 10].std()
+            additional_values[list(range(start, end, 1))] = np.random.choice([-1, 1]) * self.factor * local_std
         return additional_values

agots/multivariate_generators/multivariate_trend_outlier_generator.py
@@ -4,29 +4,13 @@ from .base import MultivariateOutlierGenerator
 
 
 class MultivariateTrendOutlierGenerator(MultivariateOutlierGenerator):
-    def __init__(self, timestamps=None, trend_value=0.06):
+    def __init__(self, timestamps=None, factor=0.06):
         self.timestamps = timestamps or []
-        self.TREND_VALUE = trend_value
-        self.trend_since = 0
-        self.ongoing_trend = False
-
-    def get_value(self, current_timestamp, timeseries):
-        start_timestamps = [start for start, _ in self.timestamps]
-        end_timestamps = [end for _, end in self.timestamps]
-
-        if current_timestamp in end_timestamps:
-            self.ongoing_trend = False
-            self.trend_since = 0
-
-        if current_timestamp in start_timestamps or self.ongoing_trend:
-            self.ongoing_trend = True
-            self.trend_since += 1
-            return np.random.random() + self.TREND_VALUE * self.trend_since
-        else:
-            return 0
+        self.factor = factor
 
     def add_outliers(self, timeseries):
-        additional_values = []
-        for timestamp_index in range(len(timeseries)):
-            additional_values.append(self.get_value(timestamp_index, None))
+        additional_values = np.zeros(timeseries.size)
+        for start, end in self.timestamps:
+            slope = self.factor * np.arange(start, end)
+            additional_values[list(range(start, end, 1))] = np.random.choice([-1, 1]) * slope
         return additional_values

agots/multivariate_generators/multivariate_variance_outlier_generator.py
@@ -4,26 +4,14 @@ from .base import MultivariateOutlierGenerator
 
 
 class MultivariateVarianceOutlierGenerator(MultivariateOutlierGenerator):
-    def __init__(self, timestamps=None, variance_factor=3):
+    def __init__(self, timestamps=None, factor=3):
         self.timestamps = timestamps or []
-        self.VARIANCE_FACTOR = variance_factor
-        self.ongoing_variance = False
-
-    def get_value(self, current_timestamp, timeseries):
-        start_timestamps = [start for start, _ in self.timestamps]
-        end_timestamps = [end for _, end in self.timestamps]
-
-        if current_timestamp in end_timestamps:
-            self.ongoing_variance = False
-
-        if current_timestamp in start_timestamps or self.ongoing_variance:
-            self.ongoing_variance = True
-            return np.random.random() * self.VARIANCE_FACTOR
-        else:
-            return 0
+        self.factor = factor
 
     def add_outliers(self, timeseries):
-        additional_values = []
-        for timestamp_index in range(len(timeseries)):
-            additional_values.append(self.get_value(timestamp_index, None))
+        additional_values = np.zeros(timeseries.size)
+        for start, end in self.timestamps:
+            difference = np.diff(timeseries[start-1:end]) if start > 0 \
+                         else np.insert(np.diff(timeseries[start:end], 0, 0))
+            additional_values[list(range(start, end, 1))] = (self.factor - 1) * difference
         return additional_values