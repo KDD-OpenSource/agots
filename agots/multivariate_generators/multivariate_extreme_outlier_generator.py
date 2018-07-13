b97c4b3032227ed0b45116e57bf4530d9cd761f9
Daniel Theveßen <danthe96@gmx.de>
2018-07-13 13:33:53 +0200

    Change how extreme values are calculated

--- agots/multivariate_generators/multivariate_extreme_outlier_generator.py
+++ agots/multivariate_generators/multivariate_extreme_outlier_generator.py
@@ -4,18 +4,14 @@ from .base import MultivariateOutlierGenerator
 
 
 class MultivariateExtremeOutlierGenerator(MultivariateOutlierGenerator):
-    def __init__(self, timestamps=None, value=None):
+    def __init__(self, timestamps=None, value=1):
         self.timestamps = [] if timestamps is None else list(sum(timestamps, ()))
         self.value = value
-        self.VALUE_FACTOR = 5
 
     def get_value(self, current_timestamp, timeseries):
         if current_timestamp in self.timestamps:
-            if self.value is None:
-                local_mean = timeseries.iloc[max(0, current_timestamp - 10):current_timestamp + 10].mean()
-                return np.random.random() + local_mean * self.VALUE_FACTOR
-            else:
-                return self.value
+            local_std = timeseries.iloc[max(0, current_timestamp - 10):current_timestamp + 10].std()
+            return np.random.choice([-1, 1]) * self.value * local_std
         else:
             return 0
 
