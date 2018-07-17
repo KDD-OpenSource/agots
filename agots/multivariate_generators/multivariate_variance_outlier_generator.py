56db50191e8ac8ea69a15acf27fb2306319fa045
Daniel Theveßen <danthe96@gmx.de>
2018-07-17 10:19:22 +0200

    Fix bracket

--- agots/multivariate_generators/multivariate_variance_outlier_generator.py
+++ agots/multivariate_generators/multivariate_variance_outlier_generator.py
@@ -12,6 +12,6 @@ class MultivariateVarianceOutlierGenerator(MultivariateOutlierGenerator):
         additional_values = np.zeros(timeseries.size)
         for start, end in self.timestamps:
             difference = np.diff(timeseries[start-1:end]) if start > 0 \
-                         else np.insert(np.diff(timeseries[start:end], 0, 0))
+                         else np.insert(np.diff(timeseries[start:end]), 0, 0)
             additional_values[list(range(start, end))] += (self.factor - 1) * difference
         return additional_values
