fcb2ef313350ef6544e48492491d8468285aaead
xasetl <xasetl@gmail.com>
2018-09-15 22:38:32 +0200

    adjust trend anomaly to normal walk

--- agots/multivariate_generators/multivariate_trend_outlier_generator.py
+++ agots/multivariate_generators/multivariate_trend_outlier_generator.py
@@ -6,7 +6,7 @@ from .base import MultivariateOutlierGenerator
 class MultivariateTrendOutlierGenerator(MultivariateOutlierGenerator):
     def __init__(self, timestamps=None, factor=8):
         self.timestamps = timestamps or []
-        self.factor = factor / 40  # Adjust for trend
+        self.factor = factor / 10  # Adjust for trend
 
     def add_outliers(self, timeseries):
         additional_values = np.zeros(timeseries.size)
